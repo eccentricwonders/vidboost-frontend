11b5bca481db0c5aa47dc7f0c964b634587e1adf
eccentricwonders <eccentricwonders@gmail.com>
2025-12-10 16:05:48 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 import App from './App';
 
-const clerkPubKey = 'pk_live_Y2xlcmsudmlkYm9vc3QuYXBwJA';
+const clerkPubKey = 'pk_live_Y2xlcmsuanNtZ2F4LmNvbSQ';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
