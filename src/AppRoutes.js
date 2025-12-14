33a5f5e81633bdfc518fde468e25315d0525c765
JSMGAX <eccentricwonders@gmail.com>
2025-12-13 23:27:45 -0500

    Add automatic scroll to top on route navigation

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import App from './App';
 import TermsPage from './TermsPage';
 import PrivacyPage from './PrivacyPage';
@@ -8,6 +8,13 @@ import FeaturesPage from './FeaturesPage';
 import AboutPage from './AboutPage';
 
 function AppRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to top on route change
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<App />} />
